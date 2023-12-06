37de14865fd5e17a889f4a895e1b8f2f290c6db3
erotolo24 <erotolo24@gmail.com>
2023-12-06 14:57:07 -0500

    new stuff

--- script.js
+++ script.js
@@ -11,14 +11,35 @@
   function WolfSpeak() {
     title.innerHTML = '<h5>Changed</h5>'
     role.textContent = 'CURRENT LEAD GUITAR'
-    desc.textContent = 'Changed'
+    desc.textContent = 'Alternative Metal/Horror Shock Rock Band from Asbury Park, NJ'
 
   }
 
   function DrugMirror() {
-    title.innerHTML = '<h5>Changed</h5>'
+    title.innerHTML = '<h5>Drug Mirror</h5>'
     role.textContent = 'CURRENT LEAD GUITAR'
-    desc.textContent = 'Changed'
+    desc.textContent = '2000's based Alternative Metal band from Asbury Park, NJ'
+
+  }
+
+  function Baquenne() {
+    title.innerHTML = '<h5>Baquenne</h5>'
+    role.textContent = 'CURRENT LEAD GUITAR'
+    desc.textContent = 'Alternative Indie Rock Band from Asbury Park, NJ'
+
+  }
+
+  function ParlorMob() {
+    title.innerHTML = '<h5>The Parlor Mob</h5>'
+    role.textContent = 'PAST FEATURED LEAD GUITAR'
+    desc.textContent = 'Alternative Hard Rock band from Asbury Park, NJ'
+
+  }
+
+  function TheColdSeas() {
+    title.innerHTML = '<h5>The Cold Seas</h5>'
+    role.textContent = 'PAST FEATURED LEAD GUITAR'
+    desc.textContent = 'Alternative Indie Rock band from Asbury Park, NJ'
 
   }
 
