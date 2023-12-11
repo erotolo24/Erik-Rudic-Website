ef5586a4eaa05b976d21665885d0692bf1b96135
erotolo24 <erotolo24@gmail.com>
2023-12-11 14:09:11 -0500

    spelling stuff

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
   // define the function to change the HTML content
   function WolfSpeak() {
-    title.innerHTML = '<h5>Changed</h5>'
+    title.innerHTML = '<h5>Wolfspeak</h5>'
     role.textContent = 'CURRENT LEAD GUITAR'
     desc.textContent = 'Alternative Metal/Horror Shock Rock Band from Asbury Park, NJ'
 
@@ -18,7 +18,7 @@
   function DrugMirror() {
     title.innerHTML = '<h5>Drug Mirror</h5>'
     role.textContent = 'CURRENT LEAD GUITAR'
-    desc.textContent = '2000's based Alternative Metal band from Asbury Park, NJ'
+    desc.textContent = '2000s based Alternative Metal band from Asbury Park, NJ'
 
   }
 
